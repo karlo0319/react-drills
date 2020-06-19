759270d8fa8219a8aa2029a07e9412c5a7e732ff
kikx0319 <karloestanislao@yahoo.com>
2020-06-18 22:28:33 -0700

    completed

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
+class App extends Component {
+  
+  constructor() {
+    super();
+  
+    this.state = {
+        inputValue: ''
+    }
+  }
+
+  handleChange(val) {
+    this.setState({inputValue: val});
+  }
+
+render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = 'App'>
+      <input className='inputLine' onChange={(e) => this.handleChange(e.target.value)} />
+      <p> {this.state.inputValue} </p>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
